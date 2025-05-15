07ef08104c273d0424c2a058230f0f97b400408f
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-15 20:56:34 -0300

    add multi-line comment example in Python

---
+++ fase-1/aula-2/ComentarioEmVariasLinhas.py
@@ -0,0 +1,10 @@
+# Esta é a primeira linha do comentário
+# Esta é a segunda linha
+# E esta é a terceira
+
+'''
+Alternativamente, strings de múltiplas linhas 
+(com três aspas simples ou duplas) são frequentemente 
+usadas como comentários multi-linha, embora tecnicamente 
+sejam strings que não estão atribuídas a nenhuma variável.
+'''
