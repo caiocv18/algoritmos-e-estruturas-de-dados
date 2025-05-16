5c76a49463f5c1ea52efe552f636b396ee12eeb3
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-15 21:06:16 -0300

    add examples for common syntax errors in Python

---
+++ fase-1/aula-2/SyntaxError.py
@@ -0,0 +1,8 @@
+# Erro: parêntese não fechado
+print("Hello World"
+# SyntaxError: unexpected EOF while parsing
+
+# Erro: dois pontos ausente em uma declaração if
+if x > 5
+    print("x é maior que 5")
+# SyntaxError: invalid syntax
