8b7e3c40283ca53e2582fa981f2006ddf6ebeb5f
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-15 21:03:17 -0300

    add examples for variable assignment in Python

---
+++ fase-1/aula-2/AtribuicaoDeVariavel.py
@@ -0,0 +1,14 @@
+# Atribuição básica
+nome = "Carlos"    # String
+idade = 30        # Integer
+pi = 3.14159      # Float
+ativo = True      # Boolean
+
+# Atribuição múltipla
+x, y, z = 1, 2, 3
+
+# Atribuição do mesmo valor a múltiplas variáveis
+a = b = c = 0
+
+# Troca de valores (sem necessidade de variável temporária)
+x, y = y, x
