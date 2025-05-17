9ba0da7ff9e21a6e2570081cecc400f9340e5eb2
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-17 16:39:41 -0300

    add interactive greeting script to enhance user engagement in Python

---
+++ fase-1/aula-2/hello_interactive.py
@@ -0,0 +1,7 @@
+# hello_interactive.py
+
+# Solicita o nome do usuário
+nome = input("Por favor, digite seu nome: ")
+
+# Imprime uma saudação personalizada
+print(f"Hello, {nome}! Bem-vindo ao mundo da programação Python!")
