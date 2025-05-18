c2382f25e6761e69498aa7c2e09d9c2c64b38a88
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:43:13 -0300

    add examples of floating-point numbers and scientific notation in Python

---
+++ fase-1/aula-3/Float.py
@@ -0,0 +1,8 @@
+# Números de ponto flutuante
+pi = 3.14159
+e = 2.71828
+temperatura = -5.7
+
+# Notação científica
+velocidade_luz = 3e8 # 3 × 10^8 (300,000,000)
+atomo = 1e-10 # 1 × 10^(-10) (0.0000000001)
