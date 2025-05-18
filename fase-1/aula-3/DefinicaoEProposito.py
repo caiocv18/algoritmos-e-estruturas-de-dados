04127c3aa168ea32be0927b3a8f2402331a55ab8
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:38:34 -0300

    add example of variable usage and future calculations in Python

---
+++ fase-1/aula-3/DefinicaoEProposito.py
@@ -0,0 +1,6 @@
+# Criando uma variável chamada 'idade' que armazena o valor 25
+idade = 25
+
+# Utilizando o valor da variável em um cálculo
+anos_no_futuro = idade + 10
+print(f"Daqui a 10 anos, você terá {anos_no_futuro} anos.")
