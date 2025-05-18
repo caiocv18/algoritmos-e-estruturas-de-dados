ce4ceee49def03097024e1dccfaaeb1fc2f6c7cb
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:59:04 -0300

    add examples of string formatting using format() method in Python

---
+++ fase-1/aula-3/Format.py
@@ -0,0 +1,9 @@
+nome = "João"
+idade = 25
+print("Olá, meu nome é {} e tenho {} anos.".format(nome, idade))
+
+# Com índices
+print("O {1} é mais velho que o {0}.".format("João", "Pedro"))
+
+# Com nomes
+print("Bem-vindo, {nome}! Você tem {idade} anos.".format(nome="Maria", idade=28))
