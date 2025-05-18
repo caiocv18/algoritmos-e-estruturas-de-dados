6164723f51739d641c078be903478beb12f8c8f8
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:40:50 -0300

    add examples of multiple assignment, sequence unpacking, and value swapping in Python

---
+++ fase-1/aula-3/AtribuicoesMultiplasEDesempacotamento.py
@@ -0,0 +1,10 @@
+# Atribuição múltipla
+x, y, z = 1, 2, 3  # x = 1, y = 2, z = 3
+
+# Desempacotamento de sequências
+coordenadas = (10, 20)
+latitude, longitude = coordenadas  # latitude = 10, longitude = 20
+
+# Troca de valores sem variável temporária
+a, b = 5, 10
+a, b = b, a  # Agora a = 10 e b = 5
