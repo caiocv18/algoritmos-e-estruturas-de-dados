4ef5035aa451cbb1951a6f00a768e21b1434c59c
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:39:24 -0300

    add examples of variable naming conventions in Python

---
+++ fase-1/aula-3/NomenclaturaEConvencoes.py
@@ -0,0 +1,9 @@
+# Bons nomes de variáveis
+idade_usuario = 25
+total_itens = 100
+esta_ativo = True
+
+# Nomes a evitar
+x = 25 # Não descritivo
+a = "João" # Não descritivo
+VariavelComNome = "Texto" # Não segue snake_case
