7b930f81bd0eb842929cff7ee3373b7a2d55ee53
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:45:43 -0300

    add examples of complex numbers and operations in Python

---
+++ fase-1/aula-3/Complex.py
@@ -0,0 +1,10 @@
+# Números complexos
+z1 = 3 + 4j # 3 (real) + 4i (imaginário)
+z2 = complex(2, -1) # 2 - 1j
+# Acessando partes real e imaginária
+print(z1.real) # 3.0
+print(z1.imag) # 4.0
+# Operações com números complexos
+print(z1 + z2) # (5+3j)
+print(z1 * z2) # (10+5j)
+print(abs(z1)) # 5.0 (magnitude do número complexo: √(3² + 4²))
