1da88061473a485c4cb91f8ea98321bbd838992d
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 17:05:38 -0300

    add examples of short-circuit evaluation with logical operators in Python

---
+++ fase-1/aula-3/AvaliacaoDeExpressoes.py
@@ -0,0 +1,4 @@
+# AND para-de avaliar no primeiro False
+print(False and print("Este print não será executado"))# False
+# OR para-de avaliar no primeiro True
+print(True or print("Este print não será executado"))# True
