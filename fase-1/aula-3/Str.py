b0f1f127a04998dbe7ac1475758f48adec642586
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:46:54 -0300

    add examples of string usage and formatting in Python

---
+++ fase-1/aula-3/Str.py
@@ -0,0 +1,14 @@
+# Strings com aspas simples
+nome = 'Maria'
+
+# Strings com aspas duplas
+mensagem = "Olá, mundo!"
+
+# Strings multilinhas com aspas triplas
+documento = """Este é um texto
+que ocupa múltiplas linhas
+no código fonte."""
+
+# Aspas dentro de strings
+frase1 = "Ele disse 'olá' para mim" # Aspas simples dentro de aspas duplas
+frase2 = 'A palavra "Python" é legal' # Aspas duplas dentro de aspas simples
