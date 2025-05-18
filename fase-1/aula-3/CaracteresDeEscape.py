b3b6eafeffb43c17afb3063d0f1b57105a800675
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:52:21 -0300

    add examples of escape characters in Python strings

---
+++ fase-1/aula-3/CaracteresDeEscape.py
@@ -0,0 +1,14 @@
+# Exemplos de caracteres de escape
+print("Uma linha\nOutra linha")  # Nova linha
+print("Nome:\tJoão")  # Tabulação
+print('Ele disse \'Olá!\'')  # Aspas simples dentro de string com aspas simples
+print("Ela disse \"Oi!\"")  # Aspas duplas dentro de string com aspas duplas
+print("C:\\Users\\Documents")  # Barra invertida
+
+# Saída:
+# Uma linha
+# Outra linha
+# Nome:    João
+# Ele disse 'Olá!'
+# Ela disse "Oi!"
+# C:\Users\Documents
