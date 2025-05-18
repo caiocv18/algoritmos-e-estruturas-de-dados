b5bd3c1d4416d5674fd501d20183e6c65be90fe7
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:41:47 -0300

    add constants for mathematical and physical values in Python

---
+++ fase-1/aula-3/Constantes.py
@@ -0,0 +1,7 @@
+PI = 3.14159
+VELOCIDADE_DA_LUZ = 299792458 # m/s
+DIAS_DA_SEMANA = 7
+
+# Mesmo sendo "constantes", tecnicamente podem ser alteradas
+# (mas não devem ser por convenção)
+PI = 3.14 # Possível, mas não recomendado
