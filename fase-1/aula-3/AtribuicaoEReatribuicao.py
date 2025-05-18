91a9a5d276675af772b583f068179a26136f8949
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-18 16:40:34 -0300

    add example of variable assignment and reassignment in Python

---
+++ fase-1/aula-3/AtribuicaoEReatribuicao.py
@@ -0,0 +1,7 @@
+# Atribuição inicial
+contador = 0
+
+# Reatribuição - incrementando o contador
+contador = contador + 1 # ou contador += 1
+# Mudando o tipo da variável (possível, mas geralmente não recomendado)
+contador = "Um"# Agora contador é uma string, não mais um número
