4fcbf70e829494cba2910ad60fc9835a81143bdb
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-19 00:56:03 -0300

    add temperature check program with conditional statements in Python

---
+++ fase-1/aula-5/Temperatura.py
@@ -0,0 +1,7 @@
+temperatura = 30
+
+if temperatura > 25:
+    print("Está calor hoje!")
+    print("Considere tomar uma água gelada.")
+
+print("Programa encerrado.")# Esta linha será executada independentemente da condição
