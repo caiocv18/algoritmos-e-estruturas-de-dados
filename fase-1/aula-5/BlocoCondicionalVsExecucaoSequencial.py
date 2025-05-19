13da4a1b722f82d1a338e6bf7acf4e278d1d8435
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-19 01:01:56 -0300

    add conditional block example for temperature check in Python

---
+++ fase-1/aula-5/BlocoCondicionalVsExecucaoSequencial.py
@@ -0,0 +1,10 @@
+temperatura = 20
+
+# Bloco condicional - executa apenas se a condição for True
+if temperatura > 25:
+    print("Está calor hoje!")
+    print("Considere tomar uma água gelada.")
+
+# Execução sequencial - sempre executa
+print("A temperatura atual é", temperatura, "graus.")
+print("Programa encerrado.")
