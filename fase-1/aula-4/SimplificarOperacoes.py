69d85191d3d83a92957da61264b8860b5405563a
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-19 00:29:07 -0300

    add examples of calculating average and finding common elements in Python

---
+++ fase-1/aula-4/SimplificarOperacoes.py
@@ -0,0 +1,14 @@
+# Cálculo de média simples
+numeros = [23, 45, 67, 89, 12]
+soma = sum(numeros)
+quantidade = len(numeros)
+media = soma / quantidade
+
+# Encontrar valores extremos
+minimo = min(numeros)
+maximo = max(numeros)
+
+# Operações com conjuntos para encontrar elementos comuns
+lista1 = [1, 2, 3, 4, 5]
+lista2 = [3, 4, 5, 6, 7]
+comuns = set(lista1).intersection(set(lista2))
