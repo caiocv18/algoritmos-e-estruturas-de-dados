8248687d7b94325e1bbed6200f18a3a16fad6d06
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-20 06:39:21 -0300

    add examples of concise conditional statements in Python

---
+++ fase-1/aula-5/CondicionaisMaisConcisas.py
@@ -0,0 +1,15 @@
+# Em vez de
+if len(lista) > 0:
+    processa_lista(lista)
+
+# Podemos escrever
+if lista:
+    processa_lista(lista)
+
+# Em vez de
+if nome != "":
+    print("Olá,", nome)
+
+# Podemos escrever
+if nome:
+    print("Olá,", nome)
