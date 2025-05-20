55b54fd1bf904c1d3adb0d5778721715c28db456
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-20 06:39:07 -0300

    add examples of values considered False in Python

---
+++ fase-1/aula-5/ValorDeVerdadeImplicito.py
@@ -0,0 +1,11 @@
+# Valores que são considerados False:
+- None
+- False
+- 0, 0.0, 0j (zero em qualquer tipo numérico)
+- "" (string vazia)
+- [] (lista vazia)
+- () (tupla vazia)
+- {} (dicionário vazio)
+- set() (conjunto vazio)
+
+# Todos os outros valores são considerados True
