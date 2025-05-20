2140d7da2e44f166e0559dcffca8c5b1443bf094
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-20 06:26:23 -0300

    add example of grading system using if-elif-else structure in Python

---
+++ fase-1/aula-5/Exemplo2.py
@@ -0,0 +1,14 @@
+nota = float(input("Digite a nota do aluno: "))
+
+if nota >= 9.0:
+    print("Conceito A")
+elif nota >= 7.0:
+    print("Conceito B")
+elif nota >= 5.0:
+    print("Conceito C")
+elif nota >= 3.0:
+    print("Conceito D")
+else:
+    print("Conceito E")
+
+print("Avaliação concluída.")
