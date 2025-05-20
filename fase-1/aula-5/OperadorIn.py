fb6318613ba8d44b8a902be7898c62a1874a0359
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-20 06:15:31 -0300

    add example of using 'in' operator for month day validation in Python

---
+++ fase-1/aula-5/OperadorIn.py
@@ -0,0 +1,7 @@
+# Ao invés de
+if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+    print("Este mês tem 31 dias")
+
+# Use
+if mes in [1, 3, 5, 7, 8, 10, 12]:
+    print("Este mês tem 31 dias")
