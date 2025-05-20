35f1e87ab818d674babe1c9a2ba9b24de308bbab
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-20 06:24:22 -0300

    add example of if-else structure in Python

---
+++ fase-1/aula-5/EstruturaCompleta.py
@@ -0,0 +1,8 @@
+if condição:
+# Bloco executado se a condição for True
+    instrução_if_1
+    instrução_if_2
+else:
+# Bloco executado se a condição for False
+    instrução_else_1
+    instrução_else_2
