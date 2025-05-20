b581e45e6ace83200c77f5d3c4663819a4a41b31
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-20 06:24:29 -0300

    add age verification example with driving eligibility in Python

---
+++ fase-1/aula-5/Exemplo.py
@@ -0,0 +1,10 @@
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Você é maior de idade.")
+    print("Pode dirigir, se possuir CNH.")
+else:
+    print("Você é menor de idade.")
+    print("Ainda não pode dirigir.")
+
+print("Verificação concluída.")# Será executado em qualquer caso
