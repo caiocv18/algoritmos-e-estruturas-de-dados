f0441947a6a571eb2da6b50ec7e7419a015f54c0
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-20 06:31:11 -0300

    add example of traditional if-else and ternary operator usage in Python

---
+++ fase-1/aula-5/Exemplo3.py
@@ -0,0 +1,8 @@
+# If-else tradicional
+if idade >= 18:
+    status = "Maior de idade"
+else:
+    status = "Menor de idade"
+
+# Usando operador ternário
+status = "Maior de idade" if idade >= 18 else "Menor de idade"
