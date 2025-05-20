9853e908641c286e6b1aaa8c0e0595a5f62edbb6
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-20 06:26:15 -0300

    add example of if-elif-else structure in Python

---
+++ fase-1/aula-5/EstruturaIfElseElif.py
@@ -0,0 +1,12 @@
+if condição_1:
+# Executado se condição_1 for True
+    bloco_1
+elif condição_2:
+# Executado se condição_1 for False e condição_2 for True
+    bloco_2
+elif condição_3:
+# Executado se condição_1 e condição_2 forem False e condição_3 for True
+    bloco_3
+else:
+# Executado se todas as condições anteriores forem False
+    bloco_else
