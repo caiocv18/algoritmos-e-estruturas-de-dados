76c1708b46e84fe3a71574beee5eaafcab8c10dc
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-21 08:41:47 -0300

    Remove examples of generating combinations, permutations, and Cartesian products in Python

--- fase-1/aula-9/GeracaoCombinacaoPermutacao1.py
+++
@@ -1,7 +0,0 @@
-# Gerando todas as combinações de 3 letras e 2 dígitos
-letras = ["A", "B", "C"]
-dígitos = [1, 2]
-
-for letra in letras:
-    for dígito in dígitos:
-        print(f"{letra}{dígito}")
