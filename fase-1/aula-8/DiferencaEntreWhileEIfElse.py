727b31c7cea1a3ddbcb81ece73a693efcbee3ab1
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-21 07:10:45 -0300

    Add example to illustrate the difference between if and while structures in Python

---
+++ fase-1/aula-8/DiferencaEntreWhileEIfElse.py
@@ -0,0 +1,9 @@
+# Estrutura if (executa no máximo uma vez)
+if condicao:
+    # Este bloco executa uma vez se condicao for True
+    realizar_acao()
+
+# Estrutura while (pode executar múltiplas vezes)
+while condicao:
+    # Este bloco executa repetidamente enquanto condicao for True
+    realizar_acao()
