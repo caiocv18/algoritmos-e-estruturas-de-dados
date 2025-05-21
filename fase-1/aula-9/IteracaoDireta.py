e37803f2c0dfeebf8a6e4535af6a9723f6de1f35
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-21 08:19:04 -0300

    Add examples of iterating over a list and a string in Python

---
+++ fase-1/aula-9/IteracaoDireta.py
@@ -0,0 +1,9 @@
+# Iterando sobre uma lista
+frutas = ["maçã", "banana", "laranja"]
+for fruta in frutas:
+    print(f"Eu gosto de {fruta}")
+
+# Iterando sobre uma string
+palavra = "Python"
+for letra in palavra:
+    print(letra.upper())
