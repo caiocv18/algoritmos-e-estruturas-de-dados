f7f873c28dd6cf0331fca25b6ea245d716670d0f
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-21 07:46:27 -0300

    Add example of processing with multiple exit conditions in a while loop

---
+++ fase-1/aula-8/MaiorControle.py
@@ -0,0 +1,9 @@
+# Processamento com múltiplas condições de saída
+while condicao_principal:
+    # Processamento...
+    if situacao_especial_1:
+        # Tratamento especial...
+        continue
+    if situacao_especial_2:
+        # Outro tratamento...
+        break
