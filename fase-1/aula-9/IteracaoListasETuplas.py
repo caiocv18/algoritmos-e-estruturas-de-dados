6827ba521d8393ac31c41f4f302ba3d674b901c1
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-21 08:21:51 -0300

    Add examples of iterating over a list and a tuple in Python

---
+++ fase-1/aula-9/IteracaoListasETuplas.py
@@ -0,0 +1,9 @@
+# Iteração em lista
+alunos = ["Ana", "Bruno", "Carlos", "Diana"]
+for aluno in alunos:
+    print(f"Aluno(a): {aluno}")
+
+# Iteração em tupla
+coordenadas = (10, 20, 30)
+for coordenada in coordenadas:
+    print(f"Coordenada: {coordenada}")
