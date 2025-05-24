55747577cb3fd57af6f5eccfc58052f0dbf46a1a
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-05-24 11:29:27 -0300

    Add example demonstrating a simple function for greeting users in Python

---
+++ fase-2/aula-1/SintaxeBasica2.py
@@ -0,0 +1,7 @@
+def saudar():
+    """Esta é uma função simples que exibe uma saudação."""
+    print("Olá! Bem-vindo ao mundo das funções em Python!")
+    print("Funções tornam nosso código mais organizado.")
+
+# Chamando a função
+saudar()
