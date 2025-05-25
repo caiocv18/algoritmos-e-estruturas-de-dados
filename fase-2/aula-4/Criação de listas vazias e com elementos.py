74063719b9fd305bbf42342ef3cadd80f7f337bf
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 11:22:17 -0300

    Add examples of creating empty and populated lists in Python

---
+++ fase-2/aula-4/Criação de listas vazias e com elementos.py
@@ -0,0 +1,14 @@
+# Lista vazia - método 1
+lista_vazia1 = []
+
+# Lista vazia - método 2
+lista_vazia2 = list()
+
+# Lista com elementos
+numeros = [1, 2, 3, 4, 5]
+nomes = ["Ana", "Bruno", "Carlos"]
+mista = [1, "Python", 3.14, True]
+
+# Lista a partir de range
+numeros_pares = list(range(0, 11, 2))# [0, 2, 4, 6, 8, 10]# Lista com repetição
+zeros = [0] * 5# [0, 0, 0, 0, 0]
