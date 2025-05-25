ca1e4390bea3175f51115a72c94b31cacc617a41
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 11:26:28 -0300

    Add example of positive indexing with a list of fruits in Python

---
+++ fase-2/aula-4/Índices positivos.py
@@ -0,0 +1,4 @@
+frutas = ["maçã", "banana", "laranja", "uva", "pera"]
+print(frutas[0])# maçã (primeiro elemento)
+print(frutas[2])# laranja (terceiro elemento)
+print(frutas[4])# pera (último elemento)
