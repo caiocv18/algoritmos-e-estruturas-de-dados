c57299eb2d785c010672e2c3cf3455bb79fe20d8
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 11:22:22 -0300

    Add examples of homogeneous lists with integers, strings, and floats in Python

---
+++ fase-2/aula-4/Listas homogêneas.py
@@ -0,0 +1,8 @@
+# Lista de inteiros
+idades = [25, 30, 22, 45, 18]
+
+# Lista de strings
+cidades = ["São Paulo", "Rio de Janeiro", "Brasília"]
+
+# Lista de floats
+precos = [19.99, 35.50, 42.00, 15.75]
