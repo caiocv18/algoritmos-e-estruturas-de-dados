7a8f1dc57c98eb7b73717252a08d4bf3a5aeaeaf
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 18:41:45 -0300

    Add example of list sorting using Python's built-in sort method

---
+++ fase-2/aula-9/sort.py
@@ -0,0 +1,4 @@
+minha_lista = [5, 1, 4, 2, 8]
+print(f"Antes do .sort(): {minha_lista}")
+minha_lista.sort() # Ordena a lista original
+print(f"Depois do .sort(): {minha_lista}") # Saída: [1, 2, 4, 5, 8]
