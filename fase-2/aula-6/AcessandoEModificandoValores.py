989ca43ee16bcc27fea08de3a3879bdb63cb5ba3
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 17:31:23 -0300

    Add example of dictionary usage with access and modification in Python

---
+++ fase-2/aula-6/AcessandoEModificandoValores.py
@@ -0,0 +1,11 @@
+# Criando um dicionário
+produto = {"nome": "Notebook", "preco": 3000, "estoque": 5}
+
+# Acessando valores
+print(produto["nome"])  # Notebook
+
+# Modificando valores
+produto["preco"] = 2800
+
+# Adicionando novos pares
+produto["marca"] = "Dell"
