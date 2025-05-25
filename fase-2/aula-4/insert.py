928bd4e1bb827d5fdaa7c0b4d9b59de7883a07b4
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 11:31:47 -0300

    Add examples of inserting elements in a list using the insert method in Python

---
+++ fase-2/aula-4/insert.py
@@ -0,0 +1,7 @@
+cores = ["vermelho", "azul", "verde"]
+cores.insert(1, "amarelo")  # Insere na posição 1
+print(cores)  # ["vermelho", "amarelo", "azul", "verde"]
+
+# Inserindo no início
+cores.insert(0, "preto")
+print(cores)  # ["preto", "vermelho", "amarelo", "azul", "verde"]
