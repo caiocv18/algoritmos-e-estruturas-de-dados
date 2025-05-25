f516c1872cb1f4b4d0e20e9935c673ca1044d73d
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 17:35:18 -0300

    Add example of accessing data in a list of dictionaries in Python

---
+++ fase-2/aula-6/ListasDeDicionarios.py
@@ -0,0 +1,9 @@
+# Lista de produtos
+produtos = [
+    {"nome": "Mouse", "preco": 50, "estoque": 20},
+    {"nome": "Teclado", "preco": 150, "estoque": 15},
+    {"nome": "Monitor", "preco": 800, "estoque": 5}
+]
+
+# Acessando dados
+print(produtos[0]["nome"])  # Mouse
