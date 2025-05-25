2351683793a1f17ea0c122b382229f478137a8cf
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 17:31:06 -0300

    Add examples of dictionary creation methods in Python

---
+++ fase-2/aula-6/CriacaoDeDicionarios.py
@@ -0,0 +1,8 @@
+# Método 1: Chaves diretas
+contatos = {"João": "1234-5678", "Maria": "9876-5432"}
+
+# Método 2: Função dict()
+cores = dict(vermelho="#FF0000", azul="#0000FF")
+
+# Método 3: Dicionário vazio
+estoque = {}
