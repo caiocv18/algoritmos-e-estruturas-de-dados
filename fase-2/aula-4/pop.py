3081c87d18b492d6cbb7efe59a85e7ec78f364ae
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 11:32:18 -0300

    Add examples of using pop method with lists in Python

---
+++ fase-2/aula-4/pop.py
@@ -0,0 +1,8 @@
+numeros = [10, 20, 30, 40, 50]
+ultimo = numeros.pop()  # Remove e retorna o último
+print(ultimo)  # 50
+print(numeros)  # [10, 20, 30, 40]
+
+primeiro = numeros.pop(0)  # Remove e retorna o primeiro
+print(primeiro)  # 10
+print(numeros)  # [20, 30, 40]
