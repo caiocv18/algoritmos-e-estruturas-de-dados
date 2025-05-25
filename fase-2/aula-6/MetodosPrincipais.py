ccc532cdbd29c94234be143591ec64a3ead90502
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 17:35:04 -0300

    Add example of using keys(), values(), and items() methods on a dictionary in Python

---
+++ fase-2/aula-6/MetodosPrincipais.py
@@ -0,0 +1,11 @@
+# Criando um dicionário para exemplo
+aluno = {"nome": "Pedro", "nota1": 8.5, "nota2": 9.0}
+
+# keys() - retorna as chaves
+chaves = aluno.keys()
+
+# values() - retorna os valores
+valores = aluno.values()
+
+# items() - retorna pares (chave, valor)
+pares = aluno.items()
