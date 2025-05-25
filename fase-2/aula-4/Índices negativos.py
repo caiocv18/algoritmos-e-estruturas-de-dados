53a9813ae8c02053e36206833b91615e1143c505
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-25 11:26:34 -0300

    Add example of negative indexing with a list of fruits in Python

---
+++ fase-2/aula-4/Índices negativos.py
@@ -0,0 +1,4 @@
+frutas = ["maçã", "banana", "laranja", "uva", "pera"]
+print(frutas[-1])# pera (último elemento)
+print(frutas[-2])# uva (penúltimo elemento)
+print(frutas[-5])# maçã (primeiro elemento)
