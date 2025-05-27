cc9e247b554760a63d09c535934463a2b97ea2bd
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-27 08:05:41 -0300

    Add example of analyzing sequential loop complexity in Python

---
+++ fase-3/aula-2/AnaliseDeLoopsSequenciais.py
@@ -0,0 +1,9 @@
+# n = tamanho da lista
+for i in range(n): # O(n)
+  # faz algo O(1)
+  pass
+
+for j in range(n * n): # O(n²)
+  # faz algo O(1)
+  pass
+# A complexidade total é O(n²), pois n² domina n.
