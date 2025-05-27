868c31fea22b5d996ef765d1570eefe9558ca268
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-27 07:40:23 -0300

    Add example of analyzing instruction sequences in Python with operation counts

---
+++ fase-3/aula-1/AnalisandoSequenciasDeInstrucoes.py
@@ -0,0 +1,5 @@
+a = 10       # 1 operação (atribuição)
+b = 20       # 1 operação
+soma = a + b # 1 operação (adição) + 1 operação (atribuição) = 2 ops
+print(soma)  # 1 operação (chamada de função/impressão)
+# Total aproximado: 5 operações. Isso não muda se 'n' for 10 ou 1 milhão.
