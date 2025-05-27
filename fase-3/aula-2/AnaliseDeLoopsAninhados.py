5a62056f44729ccf8c84ff4869f0be804dd370b9
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-27 08:06:02 -0300

    Add example of analyzing nested loops in Python with complexity O(n²)

---
+++ fase-3/aula-2/AnaliseDeLoopsAninhados.py
@@ -0,0 +1,6 @@
+# n = tamanho da lista
+for i in range(n):      # Executa n vezes
+  for j in range(n):  # Executa n vezes PARA CADA i
+    # faz algo O(1)
+    pass
+# Total: n * n = n² operações. Complexidade: O(n²)
