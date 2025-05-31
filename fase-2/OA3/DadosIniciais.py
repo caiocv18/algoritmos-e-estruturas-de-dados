833efe504b54faaedeab9162d68d496125b86f77
Caio Vinicius <caioviniciuscv18@gmail.com>
2025-05-30 23:46:10 -0300

    Add initial athlete performance data

---
+++ fase-2/OA3/DadosIniciais.py
@@ -0,0 +1,10 @@
+desempenho_atletas = [
+    ("Carlos", 7.85),
+    ("Beatriz", 8.02),
+    ("Ana", 7.91),
+    ("Daniel", 7.70),
+    ("Fernanda", 8.15),
+    ("Gustavo", 7.99),
+    ("Helena", 7.80),
+    ("Lucas", 8.05)
+]
